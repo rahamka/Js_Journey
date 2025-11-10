91c1265556600672b1f8649f864682daa52db4cc
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-11 04:22:39 +0500

    solving 39 problem.

--- Part-Two-Functions_&_Scope/script.js
+++ Part-Two-Functions_&_Scope/script.js
@@ -33,10 +33,34 @@
 
 // 35
 // Problem.  Write a function to find the maximum of three numbers.
-function largest(num1, num2, num3) {
-  console.log(Math.max(num1, num2, num3));
-}
+// function largest(num1, num2, num3) {
+//   console.log(Math.max(num1, num2, num3));
+// }
 
-largest(20, 30, 40); // Output: 40
+// largest(20, 30, 40); // Output: 40
 
 // 36
+//  Write a function that returns a factorial of a number (using a loop)
+
+// function factorialOfNum(nmbr) {}
+// factorialOfNum(20);
+
+// 37. Write a function that returns a factorial of a number (using recursion)
+
+//  38. Write a function to check if a number is prime
+// function IsPrimeNmbr(num) {
+//   console.log(num % 2 !== 0 ? "Prime" : "Not Prime");
+//   return num % 2 !== 0 ? "Prime" : "Not Prime";
+// }
+// IsPrimeNmbr(12);
+
+//  39. Write a function that reverses a string
+
+function ReverseStr(str) {
+  let value = `${str}`;
+  for (let i = 0; i < value.length; i++) {
+    console.log(value[i]);
+  }
+}
+
+ReverseStr("Hello");
