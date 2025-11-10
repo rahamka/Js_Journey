aed9a7cc64fc7310cb1e24dcd61b98618ae41ec2
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-11 03:51:31 +0500

    solved 35 problems.

---
+++ Part-Two-Functions_&_Scope/script.js
@@ -0,0 +1,42 @@
+// // 31
+// function num(sqr) {
+//   console.log(sqr * sqr);
+//   return sqr * sqr;
+// }
+
+// num(9);
+
+// // 32
+// function numbers(num1, num2) {
+//   console.log(num1 + num2);
+//   return num1 + num2;
+// }
+
+// numbers(23, 83);
+
+// 33
+// function isEven(num) {
+//   console.log(num % 2 == 0 ? "Even" : "Odd");
+//   return num % 2 == 0 ? "Even" : "Odd";
+// }
+
+// isEven(3);
+
+// 34
+
+// function nmbers(num1, num2) {
+//   console.log(num1 > num2 ? `num1 ${num1}` : `num2 ${num2}`);
+//   return num1 > num2 ? `num1 ${num1}` : `num2 ${num2}`;
+// }
+
+// nmbers(21, 20);
+
+// 35
+// Problem.  Write a function to find the maximum of three numbers.
+function largest(num1, num2, num3) {
+  console.log(Math.max(num1, num2, num3));
+}
+
+largest(20, 30, 40); // Output: 40
+
+// 36
