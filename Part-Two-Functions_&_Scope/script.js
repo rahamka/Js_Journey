37fbd107105f92f14d58de95e7c003df772b6697
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-11 10:56:01 +0500

    solved 39. problem today.

--- Part-Two-Functions_&_Scope/script.js
+++ Part-Two-Functions_&_Scope/script.js
@@ -45,7 +45,17 @@
 //  38. Write a function to check if a number is prime
 
 //  39. Write a function that reverses a string
+// function reverseString(str) {
+//   let value = [...str];
+//   value.reverse();
+//   let newStr = "";
+//   for (let items of value) {
+//     newStr += items;
+//   }
+//   console.log(`Reverse of ${str} = ${newStr}`);
+// }
 
+// reverseString("hello world");
 //  40.
 // function palindromeFun(str) {
 //   let string = `${str}`;
@@ -60,19 +70,36 @@
 // palindromeFun("hello");
 
 //  41. Write a function that returns the nth Fibonacci number
+// I was wrong here?
 // function fibonacciNumber(num) {
-//   let array = [...num];
-//   array.reduce((prev, curr) => {
-//     if (prev < curr) {
-//       console.log("fibonacci Number");
+//   let array1 = [...num];
+//   array1.reduce((prev, curr) => {
+//     if (prev + 1 == curr) {
+//       console.log("Fibonacci Number.");
+//     } else {
+//       console.log("Not Fibonacci Number.");
 //     }
 //   });
 // }
 
-// fibonacciNumber([1, 2, 3]);
+// fibonacciNumber([0, 1, 1, 2, 3, 5, 8, 13, 21]);
+// Fibonacci Number → F(n) = F(n-1) + F(n-2)
 
-// 42
-function celsiusToFahrenheit(celsius) {
-  let oneCelsisu = 33.8;
-  return oneCelsisu;
-}
+// 42. Write a function that converts a Celsius value to Fahrenheit
+// function celsiusToFahrenheit(celsius) {
+//   let oneCelsisu = 33.8;
+//   return oneCelsisu;
+// }
+
+//  43. Write a function that converts a Fahrenheit value to Celsius
+
+// 44
+// function v/s arrow function
+// Simple Function: A function which we're creating with function keyword and that function we can't assign in another variable.
+// Arrow Function: A function which we're creating with Arrow keyword(=>) and we can store the arrow function in another variable.
+
+//  45.
+
+// function defaultParameter(arg) {
+//   return arg;
+// }
