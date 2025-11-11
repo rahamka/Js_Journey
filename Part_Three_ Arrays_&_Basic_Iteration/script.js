d90b547b46baa1fa7742d92715dfcf19c3386d9c
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-11 11:33:44 +0500

    Reachout the solving rank 72.

---
+++ Part_Three_ Arrays_&_Basic_Iteration/script.js
@@ -0,0 +1,66 @@
+// 61.
+let array = [20, 30, 40, 50];
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// 62.
+// for (let elements of array) {
+//   console.log(elements);
+// }
+
+// 63.
+// for (let elements in array) {
+//   console.log(`Index = ${elements} & Element = ${array[elements]}`);
+// }
+
+// 64.
+// array.forEach((val) => {
+//   console.log(val);
+// });
+
+// 65.
+// array.map((val) => {
+//   console.log(val + val);
+// });
+
+// 66.
+// let newArray_66 = [];
+// array.filter((val) => {
+//   if (val % 2 == 0) {
+//     newArray_66.push(val);
+//   }
+// });
+// console.log(newArray_66);
+
+// 67.  Find the first element in an array that is greater than 10 using .find()
+
+// 68. Find the index of the first element that is greater than 10 using .findIndex()
+
+// 69.
+
+// array.every((val) =>
+//   val % 2 == 0 ? console.log("Positive") : console.log("Not Positive")
+// );
+
+// 70.
+
+// array.some((val) => {
+//   if (val < 0) {
+//     console.log("Negative");
+//   } else {
+//     console.log("Positive");
+//   }
+// });
+
+// 71.
+// let result_71 = array.reduce((prev, curr) => {
+//   return prev + curr;
+// });
+// console.log(result_71);
+
+// 72.
+// let result_72 = array.sort((a, b) => a - b);
+// console.log(result_72);
+
+// 73.
