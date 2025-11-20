cd43076a32e07de05787592c18de627a7c351ea6
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-20 12:06:58 +0500

    i understand 3 DOM points.

---
+++ practice.js
@@ -0,0 +1,33 @@
+// Question 1.
+// let newArr = [100, 98, 89, 69, 78];
+// newArr.map((val, idx /* array:number[] */) => {
+//   console.log(`Array No: ${idx + 1} = ${val}`);
+// here how array:number[] is working?
+// note: i know about callbackfn:, index:number?
+// define only the  array:number[] with two simple examples for me?
+// });
+
+// Question 2.
+// Is it correct for finding factorial of 4?
+// let factorialArr = [];
+// for (let i = 1; i <= 4; i++) {
+//   factorialArr.push(i);
+// }
+
+// const factorial = factorialArr.reduce((prev, curr) => {
+//   return prev * curr;
+// });
+
+// console.log(factorial);
+
+// Question 3.
+// why here this code isn't working while paras gives us list of elements?
+// let paras = document.getElementsByClassName("paras");
+// paras.forEach((val) => {
+//   console.log(val);
+// });
+
+// let paras2 = document.querySelectorAll(".paras");
+// paras2.forEach((val) => {
+//   console.log(val);
+// });
