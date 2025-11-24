8c4e773f517f5e0a5120a67f70a4d8c4b64c1403
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-24 14:04:04 +0500

    I understand the toggle button usage.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+// Today test cases for know the errors?
 let btn = document.getElementById("btn");
 let value = "Dark Mode";
-btn.onclick = () => {
-  if (value == "Dark Mode") {
-    document.body.style.backgroundColor = "black";
-    value = "Light Mode";
-    console.log("Dark Mode");
-  } else {
-    document.body.style.backgroundColor = "white";
-    value = "Dark Mode";
-    console.log("Light Mode");
-  }
-};
+
+// usage of toggle button by functions
+// this is working perfectly.
+// btn.onclick = () => {
+//   if (value == "Dark Mode") {
+//     document.body.style.backgroundColor = "black";
+//     value = "Light Mode";
+//     console.log("Dark Mode");
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     value = "Dark Mode";
+//     console.log("Light Mode");
+//   }
+// };
+
+// usage of toggle button by switch statement
+// isn't working why?
+// btn.addEventListener("click", () => {
+//   switch (value) {
+//     case "Dark Mode":
+//       document.body.style.backgroundColor = "black";
+//       value = "Light Mode";
+//       console.log("Dark Mode");
+//       break;
+//     case "Light Mode":
+//       document.body.style.backgroundColor = "white";
+//       value = "Dark Mode";
+//       console.log("Light Mode");
+//       break;
+//   }
+// });
+
+let isDark = false;
+
+btn.addEventListener("click", () => {
+  isDark = !isDark;
+  document.body.style.backgroundColor = isDark ? "black" : "white";
+});
