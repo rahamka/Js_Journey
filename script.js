a62618ccae75a7f3277f74568ac54c2b5bda4a2f
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-24 14:47:48 +0500

    started to understand the events in JavaScript.

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
-// Today test cases for know the errors?
+// target , clientX & clientY
 let btn = document.getElementById("btn");
-let value = "Dark Mode";
 
-// usage of toggle button by functions
-// this is working perfectly.
-// btn.onclick = () => {
-//   if (value == "Dark Mode") {
-//     document.body.style.backgroundColor = "black";
-//     value = "Light Mode";
-//     console.log("Dark Mode");
-//   } else {
-//     document.body.style.backgroundColor = "white";
-//     value = "Dark Mode";
-//     console.log("Light Mode");
-//   }
-// };
-
-// usage of toggle button by switch statement
-// isn't working why?
-// btn.addEventListener("click", () => {
-//   switch (value) {
-//     case "Dark Mode":
-//       document.body.style.backgroundColor = "black";
-//       value = "Light Mode";
-//       console.log("Dark Mode");
-//       break;
-//     case "Light Mode":
-//       document.body.style.backgroundColor = "white";
-//       value = "Dark Mode";
-//       console.log("Light Mode");
-//       break;
-//   }
-// });
-
-let isDark = false;
-
-btn.addEventListener("click", () => {
-  isDark = !isDark;
-  document.body.style.backgroundColor = isDark ? "black" : "white";
+btn.addEventListener("click", (evt) => {
+  console.log(evt.x);
+  if (evt.x == "16px") {
+    console.log("hello world");
+  }
 });
