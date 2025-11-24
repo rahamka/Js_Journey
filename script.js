cd5fa3384962e173da96664503e1a5a55db8ff5f
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-24 13:44:05 +0500

    understand the events properties.

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// removeChild
-let wholeDiv = document.getElementById("wholeDiv");
-let box_3 = document.getElementById("box-3");
-wholeDiv.removeChild(box_3);
-
-// appendChild
-let h1 = document.createElement("h1");
-h1.textContent = "Heading";
-wholeDiv.appendChild(h1);
-
-// practice question
-let button = document.createElement("button");
-button.textContent = "Click me!";
-button.style.background = "red";
-button.style.color = "white";
-
-let body = document.getElementById("body");
-body.prepend("button");
-
-// practice question - 2
-let el = document.getElementById("para");
-el.classList.add("newClass");
+let btn = document.getElementById("btn");
+let value = "Dark Mode";
+btn.onclick = () => {
+  if (value == "Dark Mode") {
+    document.body.style.backgroundColor = "black";
+    value = "Light Mode";
+    console.log("Dark Mode");
+  } else {
+    document.body.style.backgroundColor = "white";
+    value = "Dark Mode";
+    console.log("Light Mode");
+  }
+};
