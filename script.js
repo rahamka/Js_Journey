9cba2f9fca547db4e9d67718ab3ae81e5bf9f63f
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-11-25 02:55:14 +0500

    completed 5 questions.

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-// target , clientX & clientY
+// task 1
 let btn = document.getElementById("btn");
-let value = 34;
+let value = false;
+btn.addEventListener("click", () => {
+  if (value == false) {
+    document.body.style.backgroundColor = "black";
+    value = true;
+  } else {
+    document.body.style.backgroundColor = "white";
+    value = false;
+  }
+});
+
+// task 1 way = > 2
 
 btn.addEventListener("click", () => {
-  console.log(!Number.isNaN(value));
+  value = !value;
+  document.body.style.backgroundColor = value ? "black" : "white";
+});
+
+// task 2
+let mousePara = document.getElementById("mousemovePara");
+mousePara.addEventListener("mousemove", (evt) => {
+  mousePara.innerText = `clientX = ${evt.x}, & clientY = ${evt.y}`;
+});
+
+// task 3
+let inputField = document.getElementById("input");
+inputField.addEventListener("keydown", (evt) => {
+  if (evt.key == "Enter") {
+    inputField.value = `Entered total ${inputField.value.length} chars.`;
+  }
+});
+
+// note: i can append the innerField in another element but i want to simple get results.
+
+// task 4.
+let img = document.getElementById("myImg");
+
+img.addEventListener("dblclick", () => {
+  value = !value;
+  img.style.width = value ? "150px" : "100px";
+  img.style.height = value ? "150px" : "100px";
+});
+
+// task 5.
+let submitBtn = document.getElementById("form");
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  let name = form.querySelector("input").value;
+  alert("Hello " + name + "!");
 });
